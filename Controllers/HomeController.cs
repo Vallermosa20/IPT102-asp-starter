1dcbea8a385647886c76bb486a3e91f4a6861479
Vallermosa20 <angel.vallermosa@gmail.com>
2023-10-06 15:33:13 +0800

    Vallermosa20/IPT102-asp-starter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,11 @@ public class HomeController : Controller
             new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
             new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
             new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+            new StudentInfoModel { Name = "Angel Vallermosa", StudentId = "20-1298", StudentAge = "21"}
+            new StudentInfoModel { Name = "Feona Gana", StudentId = "21-2177", StudentAge = "21"}
+            new StudentInfoModel { Name = "Alesa Tabios", StudentId ="21-2097", StudentAge = "20"}
+
+
         };
 
         return View(studentInfoArray);
