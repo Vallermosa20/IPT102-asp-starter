e171824729073160a56e22304d4b9c27e2b3dc6d
Vallermosa20 <angel.vallermosa@gmail.com>
2023-10-06 16:25:01 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,15 +2,17 @@
 
 public class HomeController : Controller
 {
+   
     public IActionResult Index()
     {
+        
         var studentInfoArray = new[]
         {
             new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
             new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
             new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
-            new StudentInfoModel { Name = "Angel Vallermosa", StudentId = "20-1298", StudentAge = "21"}
-            new StudentInfoModel { Name = "Feona Gana", StudentId = "21-2177", StudentAge = "21"}
+            new StudentInfoModel { Name = "Angel Vallermosa", StudentId = "20-1298", StudentAge = "21"},
+            new StudentInfoModel { Name = "Feona Gana", StudentId = "21-2177", StudentAge = "21"},
             new StudentInfoModel { Name = "Alesa Tabios", StudentId ="21-2097", StudentAge = "20"}
 
 
